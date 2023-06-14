d4ff856d531ead3d4f6d1e219a2c2cf82790e5c1
Sumitgautam9911 <sumit@krenai.com>
2023-06-14 14:26:01 +0530

    config ui

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ConfigurationComponent } from './configuration/configuration.component';
+
+const routes: Routes = [{ path: 'configuration', component:ConfigurationComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
