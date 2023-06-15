21148f1ec4d3222796cd7e52092cbe8b979ba53d
D K BIND <dkbind@krenai.com>
2023-06-15 13:00:39 +0530

    fxs

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DatabaseHelper } from '../models/DatabaseHelper';
+import { LookupTaxonomy } from '../models/LookupTaxonomy';
 
 @Component({
   selector: 'app-configuration',
@@ -10,12 +12,23 @@ export class ConfigurationComponent implements OnInit {
 
   constructor(private _router: Router) { }
 
+  databaseHelper:DatabaseHelper = new DatabaseHelper();
+  lookupTaxonomyList:LookupTaxonomy[] = new Array();
+  totalLookupTaxonomy : number = 20;
+
   ngOnInit(): void {
   }
 
   logOut() {
     localStorage.clear();
     this._router.navigate(['/auth/login']);
+  }
+
+  pageChanged(event: any) {
+    if (event != this.databaseHelper.currentPage) {
+      this.databaseHelper.currentPage = event;
+      // this.getAllProvider();
+    }
   }
 
 }
