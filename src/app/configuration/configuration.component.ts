b8d119e3685347db07d954939897e1f70fd37519
D K BIND <dkbind@krenai.com>
2023-06-15 13:28:05 +0530

    fxs

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DatabaseHelper } from '../models/DatabaseHelper';
 import { LookupTaxonomy } from '../models/LookupTaxonomy';
+import { LookupTaxonomyService } from '../services/lookup-taxonomy.service';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-configuration',
@@ -10,11 +12,14 @@ import { LookupTaxonomy } from '../models/LookupTaxonomy';
 })
 export class ConfigurationComponent implements OnInit {
 
-  constructor(private _router: Router) { }
+  constructor(private _router: Router,
+    private lookupTaxonomyService:LookupTaxonomyService,
+    private dataService:DataService) { }
 
   databaseHelper:DatabaseHelper = new DatabaseHelper();
   lookupTaxonomyList:LookupTaxonomy[] = new Array();
   totalLookupTaxonomy : number = 20;
+  loadingLookupTaxonomy:boolean = false;
 
   ngOnInit(): void {
   }
@@ -24,10 +29,21 @@ export class ConfigurationComponent implements OnInit {
     this._router.navigate(['/auth/login']);
   }
 
+  getLookupTaxonomyMap(){
+    this.loadingLookupTaxonomy = true;
+    this.lookupTaxonomyService.getLookupTaxonomyMap(this.databaseHelper).subscribe(resp=>{
+
+      this.loadingLookupTaxonomy = false;
+    },error=>{
+      this.loadingLookupTaxonomy = false;
+      this.dataService.showToast(error.error);
+    })
+  }
+
   pageChanged(event: any) {
     if (event != this.databaseHelper.currentPage) {
       this.databaseHelper.currentPage = event;
-      // this.getAllProvider();
+      this.getLookupTaxonomyMap();
     }
   }
 
---
+++ src/app/services/lookup-taxonomy.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Keys } from '../models/key';
+import { Observable } from 'rxjs';
+import { DatabaseHelper } from '../models/DatabaseHelper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LookupTaxonomyService {
+
+  constructor(private http: HttpClient) { }
+
+  key: Keys = new Keys();
+
+  getLookupTaxonomyMap(databaseHelper:DatabaseHelper): Observable<any> {
+    if(databaseHelper==undefined || databaseHelper==null){
+      databaseHelper = new DatabaseHelper();
+    }
+    const params = new HttpParams()
+    .set('search', databaseHelper.search)
+    .set('searchBy', databaseHelper.searchBy)
+    .set('currentPage', databaseHelper.currentPage)
+    .set('itemsPerPage', databaseHelper.itemsPerPage)
+
+    return this.http.get<any>(this.key.server_url + this.key.api_version_one + this.key.lookup_taxonomy, {params});
+  }
+
+}
