49140d024ff2a29fabd1a453416da5957860e33c
suman singh <sumansingh@sumans-MacBook-Pro.local>
2023-06-19 14:28:55 +0530

    some fixes

--- src/app/models/key.ts
+++ src/app/models/key.ts
@@ -3,7 +3,7 @@ export class Keys{
     /**
      * Server url key
      */
-    server_url: string = "http://localhost:8080"; 
+    server_url: string = "http://localhost:8081"; 
 
     api_version_one = "/api/v1";
     login = '/auth/login'
