5040c2c8387d7c9158f025cb5ceb001e4ae065b2
shivam <shivam.bharti@krenai.com>
2023-06-20 15:39:46 +0530

    user integraton

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Keys } from '../models/key';
+import { UserAccountRequest } from '../models/UserAccountRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -38,6 +39,22 @@ export class AuthService {
     .set('userName', userName)
     .set('oldPassword', oldPassword)
     .set('newPassword', newPassword)
-    return this.http.patch<any>(this.key.server_url + this.key.api_version_one + "/auth/update-password", {},{params});
+    return this.http.patch<any>(this.key.server_url + this.key.api_version_one + "/auth/update-password", {}, {params});
   }
+
+  createUser(userAccountRequest:any): Observable<any>{
+    return this.http.post<any>(this.key.server_url + this.key.api_version_one + "/auth/create", userAccountRequest);
+  }
+
+  getAllUser(): Observable<any>{
+    return this.http.get<any>(this.key.server_url + this.key.api_version_one + "/auth/get-user")
+  }
+
+  // updateUserStatus(user : UserAccountRequest): Observable<any> {
+  //   const params = new HttpParams()
+  //   .set('userName', user.userName)
+  //   .set('status', user.status)
+
+  //   return this._http.patch<any>(this.Key.host_url+this.Key.enterprise_leader + "/status-update",{},{params});
+  // }
 }
