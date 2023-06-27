315625df1fcb7fe645c4cfd4a16e02265c76632f
D K BIND <dkbind@krenai.com>
2023-06-27 11:48:53 +0530

    fxs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
-import { ConfigurationListingComponent } from './configuration-listing/configuration-listing.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
 import { AccountUserComponent } from './account-user/account-user.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'configuration', component: ConfigurationComponent },
-  { path: 'configuration-listing', component: ConfigurationListingComponent },
   { path: 'account-user', component: AccountUserComponent },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
 
