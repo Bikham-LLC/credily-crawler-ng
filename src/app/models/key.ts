a79dd00d6e990d99adecd1543e9adaaa24fb5f25
shivaa00 <mac@MACs-MacBook-Pro.local>
2023-07-11 09:53:42 +0530

    fxs

--- src/app/models/key.ts
+++ src/app/models/key.ts
@@ -3,8 +3,8 @@ export class Keys{
     /**
      * Server url key
      */
-    // server_url: string = "http://localhost:8081"; 
-    server_url: string = "http://ec2-43-204-22-113.ap-south-1.compute.amazonaws.com:8081"; 
+    server_url: string = "http://localhost:8081"; 
+    // server_url: string = "http://ec2-43-204-22-113.ap-south-1.compute.amazonaws.com:8081"; 
 
     api_version_one = "/api/v1";
     login = '/auth/login'
