27e155704a7d4e681ff65a15076f002979d37926
shivaa00 <mac@MACs-MacBook-Pro.local>
2023-07-27 18:58:12 +0530

    fxs

--- src/app/models/Constant.ts
+++ src/app/models/Constant.ts
@@ -3,9 +3,9 @@
 export class Constant{
 
     static EMPTY_STRINGS = [null, undefined, "", "N/A", "n/a", " ", "null"];
-	static LOOKUP_HIDE = [5,6,8,10,17,19,21,23,24];
-	static EVENT_HIDE = [7,8,10,19,21,23,24];
-	static ENTITY_HIDE = [4,6,19,23,24];
+	static LOOKUP_HIDE = [5,6,8,10,17,19,21,23,24,25];
+	static EVENT_HIDE = [7,8,10,19,21,23,24,25];
+	static ENTITY_HIDE = [4,6,19,23,24,25];
 
     /**
 	 * Authentication constants
