288c6885e570f8b5ccb14912fbdac9dac19b771e
shivaa00 <mac@MACs-MacBook-Pro.local>
2023-10-06 14:59:42 +0530

    crop screenshot add

--- src/app/models/Constant.ts
+++ src/app/models/Constant.ts
@@ -4,8 +4,8 @@ export class Constant{
 
     static EMPTY_STRINGS = [null, undefined, "", "N/A", "n/a", " ", "null"];
 	static LOOKUP_HIDE = [5,6,8,10,17,19,22,23,24,25];
-	static EVENT_HIDE = [7,8,10,19,23,24,25,27];
-	static ENTITY_HIDE = [4,6,19,23,24,25];
+	static EVENT_HIDE = [7,8,10,19,23,24,25,27,32];
+	static ENTITY_HIDE = [4,6,19,23,24,25,32];
 
 	static CRAWLER_TYPE_LICENSE_LOOKUP = 'License Lookup';
 	static CRAWLER_TYPE_BOARD_CERTIFICATION = 'Board Certification';
