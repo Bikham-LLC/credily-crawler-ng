ce8b22a91af5b0024e2f56db5503b13c3c464cce
shivam.bharti <shivam.bharti@krenai.com>
2024-02-06 14:57:05 +0530

    dashboard integration

--- src/app/models/DashboardV2ConfigDataList.ts
+++ src/app/models/DashboardV2ConfigDataList.ts
@@ -1,9 +1,27 @@
 export class DashboardV2ConfigDataList{
-    completedConfig:number=0;
+    
     totalProvidersAdded:number=0;
-    failedConfig:number=0;
-    noConfigFound:number=0;
-    totalConfigs:number=0;
+    
+    boardCompletedConfig:number=0;
+    boardFailedConfig:number=0;
+    boardNoConfigFound:number=0;
+    boardTotalConfigs:number=0;
+    boardReRunPending:number=0;
+    boardReRunFailed:number=0;
+
+    licenseCompletedConfig:number=0;
+    licenseFailedConfig:number=0;
+    licenseNoConfigFound:number=0;
+    licenseTotalConfigs:number=0;
+    licenseReRunPending:number=0;
+    licenseReRunFailed:number=0;
+
+    commonCompletedConfig:number=0;
+    commonFailedConfig:number=0;
+    commonNoConfigFound:number=0;
+    commonTotalConfigs:number=0;
+    commonReRunPending:number=0;
+    commonReRunFailed:number=0;
 
     taxState:string='';
     count:number=0
