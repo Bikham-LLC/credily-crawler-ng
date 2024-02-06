a7fe0b985f598a6524287057c6b67c6bf7b91ae9
Sumitgautam9911 <sumit@krenai.com>
2024-02-06 17:26:17 +0530

    report Ui Design

--- src/app/reports/reports-routing.module.ts
+++ src/app/reports/reports-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FailedConfigReportComponent } from './components/failed-config-report/failed-config-report.component';
+import { NoconfigFoundReportComponent } from './components/noconfig-found-report/noconfig-found-report.component';
+import { ProviderReportComponent } from './components/provider-report/provider-report.component';
+import { TestReportComponent } from './components/test-report/test-report.component';
 import { ReportsComponent } from './reports.component';
 
 const routes: Routes = [{ 
   path: '', component: ReportsComponent,
   children:[
-    {path: 'failed-cofing-report', component: FailedConfigReportComponent}
+    {path: 'failed-cofing-report', component: FailedConfigReportComponent},
+    {path: 'provider-report', component: ProviderReportComponent},
+    {path: 'test-report', component: TestReportComponent},
+    {path: 'noconfig-found-report', component: NoconfigFoundReportComponent}
   ] 
 
 }];
