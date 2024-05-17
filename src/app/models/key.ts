4d9759def225f53de275b3dcd41fcf8996591cad
shivam.bharti <shivam.bharti@krenai.com>
2024-05-17 18:47:12 +0530

    fixes

--- src/app/models/key.ts
+++ src/app/models/key.ts
@@ -3,8 +3,8 @@ export class Keys{
     /**
      * Server url key
      */
-    // server_url: string = "https://crawler.providerpassport.info";
-    server_url: string = "http://localhost:8081";
+    server_url: string = "https://crawler.providerpassport.info";
+    // server_url: string = "http://localhost:8081";
 
 
     api_version_one = "/api/v1";
@@ -14,7 +14,7 @@ export class Keys{
     lookup_crawler_attribute = '/license-lookup-configuration/attribute'
     crewler_controller = '/web-crawler'
     provider_crawler_controller = '/provider-crawler'
-    queue = '/aws';
+    queue = '/queue';
     report = '/report'
     dashboard = '/dashboard'
     
