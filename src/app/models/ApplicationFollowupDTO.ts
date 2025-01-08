5bf5902b328094d7c9b062a74ccc703a19aa8bff
Amit-Krenai <amit-krenai@Mrs-MacBook-Pro.local>
2025-01-08 18:51:29 +0530

    followup implemented

---
+++ src/app/models/ApplicationFollowupDTO.ts
@@ -0,0 +1,12 @@
+export class ApplicationFollowupDTO {
+
+    updatedDate:any;
+    ticketId:number=0;
+    ticketType:string='';
+    planId:number=0;
+    status:string='';
+    url:string='';
+    lookupId:number=0;
+    lookupName:string='';
+    lookupLink:string='';
+}
--- src/app/reports/components/follow-up-report/follow-up-report.component.ts
+++ src/app/reports/components/follow-up-report/follow-up-report.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { ApplicationFollowupDTO } from 'src/app/models/ApplicationFollowupDTO';
+import { Constant } from 'src/app/models/Constant';
+import { DatabaseHelper } from 'src/app/models/DatabaseHelper';
+import { Route } from 'src/app/models/Route';
+import { RpaReportDTO } from 'src/app/models/RpaReportDTO';
+import { DataService } from 'src/app/services/data.service';
+import { HeaderSubscriptionService } from 'src/app/services/header-subscription.service';
+import { ReportService } from 'src/app/services/report.service';
 
 @Component({
   selector: 'app-follow-up-report',
@@ -7,9 +18,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FollowUpReportComponent implements OnInit {
 
-  constructor() { }
+ 
+  readonly Route = Route;
+  readonly Constant = Constant;
+
+  constructor(private reportService : ReportService,
+    private dataService: DataService,
+    private router : Router,
+    private headerSubscriptionService: HeaderSubscriptionService) { 
+
+      this.providerSearch.pipe(
+        debounceTime(600))
+        .subscribe(value => {
+          this.databaseHelper.currentPage = 1;
+          this.getFollowUpReport();
+        });
+  
+        this.subscribeHeader = this.headerSubscriptionService.headerVisibilityChange.subscribe(async (value) => {
+          debugger
+          if(router.url == Route.FOLLOW_UP_REPORT){
+            this.getFollowUpReport();
+          }
+        })
+
+    }
 
   ngOnInit(): void {
+    this.getFollowUpReport()
+  }
+
+  subscribeHeader:any;
+  
+
+  providerSearch = new Subject<string>();
+  totalProviders:number = 0;
+  databaseHelper: DatabaseHelper = new DatabaseHelper();
+  followupList: ApplicationFollowupDTO[] = new Array();
+  reportLoadingToggle:boolean = false;
+
+
+  getFollowUpReport(){
+    this.reportLoadingToggle = true;
+    this.reportService.getFollowUpReport(this.databaseHelper, this.dataService.startDate, this.dataService.endDate).subscribe(response=>{
+      if(response.status){
+        this.followupList = response.object;
+        this.totalProviders = response.totalItems;
+      }
+
+      this.reportLoadingToggle = false;
+    },error=>{
+      this.reportLoadingToggle = false;
+    })
+  }
+
+  pageChange(event:any){
+    this.databaseHelper.currentPage = event;
+    this.getFollowUpReport();
+  }
+
+  imageUrl:string='';
+  imageLoadingToggle:boolean = false;
+  @ViewChild('openSnapshotModalButton') openSnapshotModalButton!: ElementRef
+  openImage(url:string){
+    this.imageLoadingToggle = true;
+    this.openSnapshotModalButton.nativeElement.click();
+    this.imageUrl = url;
+    setTimeout(() => {
+      this.imageLoadingToggle = false;
+    }, 1000);
   }
 
 }
